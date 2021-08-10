31f2c8aa4449319a2257052bf751a79d25064d7f
alfredsmarteze <alfredsmarteze@outlook.com>
2021-08-10 23:49:01 +0100

    Add project files.

---
+++ RichyProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RichyProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
